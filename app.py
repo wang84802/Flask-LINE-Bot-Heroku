c92d0b96963551bae6e938b4d6e0477b2f0a5703
Chris <wang84802@gmail.com>
2022-05-17 17:43:46 +0800

    update return message

--- app.py
+++ app.py
@@ -33,9 +33,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    get_message = event.message.text
-
-    # Send To Line
-    reply = TextSendMessage(text=f"{get_message}")
-    line_bot_api.reply_message(event.reply_token, reply)
-    line_bot_api.reply_message(event.reply_token, 'first change')
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)
+    )
