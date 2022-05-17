c99eb30d65fa21163843c44a3a2a80e4b503a231
Chris <wang84802@gmail.com>
2022-05-17 17:18:31 +0800

    Change README.md

--- app.py
+++ app.py
@@ -37,3 +37,5 @@ def handle_message(event):
 
     # Send To Line
     reply = TextSendMessage(text=f"{get_message}")
+    line_bot_api.reply_message(event.reply_token, reply)
+    line_bot_api.reply_message(event.reply_token, 'first change')
